78484a3b2169da03f3fa461bbf74cb1b127955ce
Annie Chen <annie.chen@tufts.edu>
2017-12-11 13:22:29 +0000

    parser for the rooms and furniture

---
+++ parser.py
@@ -0,0 +1,25 @@
+
+def parse(program):
+    with open(program) as f:
+        line = f.readline()
+        line = f.readline()
+#        for line in f:
+
+        beg = line.split("#")[0]
+        problem_num = beg.split(":")[0]
+        room = beg.split(":")[1]
+        all_shapes = line.split("#")[1].split(";")
+        
+        print "Problem", problem_num
+        print "Room vertices", room
+
+        for i in range(0, len(all_shapes)):
+            shape = all_shapes[i].split(":");
+            cost = shape[0];
+            vertices = shape[1];
+            print i, "Cost is", cost, "for this shape", vertices
+            # instead of printing, store into our polygon data structure
+            # there are a lot of duplicates so avoid storing duplicates
+
+program = "input.txt"
+parse(program)
