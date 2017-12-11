7c063b5deba4e1ce7ca334ebefd1f191432a16e5
Annie Chen <annie.chen@tufts.edu>
2017-12-11 13:24:29 +0000

    reading first line now

--- parser.py
+++ parser.py
@@ -2,9 +2,7 @@
 def parse(program):
     with open(program) as f:
         line = f.readline()
-        line = f.readline()
 #        for line in f:
-
         beg = line.split("#")[0]
         problem_num = beg.split(":")[0]
         room = beg.split(":")[1]
@@ -19,7 +17,8 @@ def parse(program):
             vertices = shape[1];
             print i, "Cost is", cost, "for this shape", vertices
             # instead of printing, store into our polygon data structure
-            # there are a lot of duplicates so avoid storing duplicates
+            # there are a lot of duplicates so avoid storing duplicates unless
+            # intended
 
 program = "input.txt"
 parse(program)
