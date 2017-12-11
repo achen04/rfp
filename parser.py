0149281afaee73162c27feb585e87cd22c3660ac
Annie Chen <annie.chen@tufts.edu>
2017-12-11 15:53:06 +0000

    adapted to python 3

--- parser.py
+++ parser.py
@@ -2,20 +2,21 @@
 def parse(program):
     with open(program) as f:
         line = f.readline()
+
 #        for line in f:
         beg = line.split("#")[0]
         problem_num = beg.split(":")[0]
         room = beg.split(":")[1]
         all_shapes = line.split("#")[1].split(";")
         
-        print "Problem", problem_num
-        print "Room vertices", room
+        print("Problem", problem_num)
+        print("Room vertices", room)
 
         for i in range(0, len(all_shapes)):
             shape = all_shapes[i].split(":");
             cost = shape[0];
             vertices = shape[1];
-            print i, "Cost is", cost, "for this shape", vertices
+            print(i, "Cost is", cost, "for this shape", vertices)
             # instead of printing, store into our polygon data structure
             # there are a lot of duplicates so avoid storing duplicates unless
             # intended
